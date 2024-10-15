c1cb838f1d1ad22446b0511b34e6e1b6edd87822
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-15 17:19:30 +0900

    Solve

--- agc013_a.jl
+++ agc013_a.jl
@@ -1,1 +1,32 @@
+parseint() = readline() |> x -> parse(Int, x)
 
+parsestring() = readline()
+
+parseints() = readline() |> split |> x -> parse.(Int, x)
+
+parsestrings() = readline() |> split
+
+function outcome()
+    n = parseint()
+    an = parseints()
+
+    outcome = 0
+
+    i = 1
+    while i < length(s)+1
+        j = i
+
+        # while j < length(s)+1 && s[i] == s[j]
+        #     j += 1
+        # end
+
+        k_up = k_down = j
+
+        push!(arr, (s[i], j-i))
+        i = j
+    end
+
+    println(outcome)
+end
+
+outcome()
