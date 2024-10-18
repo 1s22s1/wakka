1e564678ad8f66dcb073292e41c9f8117c356b23
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-19 06:48:07 +0900

    :up:

--- 1019/cumulative_sum.jl
+++ 1019/cumulative_sum.jl
@@ -6,4 +6,8 @@ for i ∈ 1:3, j ∈ 1:3
     s[i+1, j+1] = s[i+1, j] + s[i, j+1] - s[i, j] + a[i, j]
 end
 
+x1 = y1 = 2
+x2 = y2 = 3
+
 @show s
+@show s[x2+1, y2+1] - s[x2+1, y1] - s[x1, y2+1] + s[x1, y1]
