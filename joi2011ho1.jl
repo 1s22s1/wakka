6a1615524596f2cbf5cd83eac7bd02f2b7947560
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-19 07:04:04 +0900

    wip

--- joi2011ho1.jl
+++ joi2011ho1.jl
@@ -9,10 +9,20 @@ function main()
         push!(map, line)
     end
 
+    sj = zeros(Int, m + 1, n + 1)
+    oj = zeros(Int, m + 1, n + 1)
 
+    for i ∈ 1:m, j ∈ 1:n
+        cur_j = map[i][j] == "J" ? 1 : 0
+        sj[i+1, j+1] = sj[i+1, j] + sj[i, j+1] - sj[i, j] + cur_j
+    end
 
     for _ ∈ 1:k
         a, b, c, d = parseints()
+
+        outcome_j = sj[c+1, d+1] - sj[c+1, b] - sj[a, d+1] + sj[a, b]
+
+        println("$(outcome_j)")
     end
 end
 
